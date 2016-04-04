1a5c7a7c279fb6b61535a584b3dcbe59e7ce3503
VS3XXBD <VS3XXBD@VS3XXBD08ADMXCO.telcel.net>
2016-04-04 14:55:10 -0500

    Se rehace clase administradoresSAP

--- src/MonSAP/AdministradoresSAP.java
+++ src/MonSAP/AdministradoresSAP.java
@@ -5,19 +5,106 @@
  */
 package MonSAP;
 
+import Monitoreos.Conexion;
+import Monitoreos.ExecQuery;
+import java.sql.ResultSet;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author VS3XXBD
  */
 public class AdministradoresSAP extends javax.swing.JFrame {
-
+    String cadenaBD;
+    ResultSet AdmUsrAdm;
     /**
      * Creates new form AdministradoresSAP
      */
-    public AdministradoresSAP() {
+    public AdministradoresSAP(String cadenaBD) {
+        this.cadenaBD = cadenaBD;
         initComponents();
+        definirModelosAdminUsrAdmin();
     }
 
+    
+    void definirModelosAdminUsrAdmin()                                               //Define el modelo de la tabla de inconsistencias de usuarios internos reportados como baja
+    {
+        DefaultTableModel modeloBajasInt = new DefaultTableModel();             //Crea el objeto modelo de tabla
+        
+        
+        
+        Object[] registro = new Object[6];                                     //Crea un arreglo para recibir los elementos de cada renglon
+        int i = 0;                                                              //Inicializa la variable para el contador
+        try
+        {
+            Conexion conLocal = new Conexion();                                 //Inicia la conexión local
+//            System.out.println("CadenaBD " + cadenaBD);
+            conLocal.AbrirLocal(cadenaBD);
+            ExecQuery EjecutaLo = new ExecQuery();                              //Crea el objeto para ejecutar la consulta
+            AdmUsrAdm = EjecutaLo.Cons(conLocal.conexion, Monitoreos.Querys.AdministraUsuariosAdmin());   //Ejecuta la consulta y almacena el resultado en la variable
+            
+            if(AdmUsrAdm.next())                                                 //Verifica que el resultado no esté vacío
+            {
+                modeloBajasInt.addColumn("Permitir");                            //Crea las columnas necesarias para el reporte
+                modeloBajasInt.addColumn("Usuario");
+                modeloBajasInt.addColumn("Nombre");
+                modeloBajasInt.addColumn("Apellido");
+                modeloBajasInt.addColumn("Rol");
+                modeloBajasInt.addColumn("Valor de autorización");
+                AdmUsrAdm.beforeFirst();                                         //Regresa a la posición inicial del resultado
+                while(AdmUsrAdm.next())                                          //Lee cada registro hasta que ya no haya más
+                {
+                    for(int k=1; k<7; k++)                                     
+                    {
+//                        System.out.println("index " + k);
+                        if(k==1)                                                
+                        {
+                            registro[k-1]=AdmUsrAdm.getBoolean(k);                         //Si está en la primer columna establece un valor TRUE para que el checkbox esté seleccionado
+                        }
+                        else
+                        {
+                            registro[k-1]=AdmUsrAdm.getString(k);              //Recorre los elementos del registro para obtener cada dato de las columnas
+                        }
+                    }
+                    modeloBajasInt.addRow(registro);                            //Ya que todos los elementos del registro están en el arreglo se agrega el arreglo como un nuevo renglón de la tabla
+                }
+
+                TablaAdminUsrAdmin.setModel(modeloBajasInt);                         //Una vez construida completamente la tabla se define el modelo a la tabla original
+                TablaAdminUsrAdmin.setAutoResizeMode(AUTO_RESIZE_OFF);               //Se desabilita el ajuste automatico de ancho de columnas para establecerlo manualmente
+                TablaAdminUsrAdmin.getColumnModel().getColumn(0).setCellEditor(new Clase_CellEditor());  //Se hace uso de editor y renderizador de columnas
+                TablaAdminUsrAdmin.getColumnModel().getColumn(0).setCellRenderer(new Clase_CellRender());
+
+                TableColumn CAgregar = TablaAdminUsrAdmin.getColumn("Permitir"); //Se llama a la columna
+                CAgregar.setPreferredWidth(60);                                 //Se define su tamaño
+                TableColumn Usuario = TablaAdminUsrAdmin.getColumn("Usuario");  //Se llama a la columna
+                Usuario.setPreferredWidth(120);                                 //Se define su tamaño
+                TableColumn Nombre = TablaAdminUsrAdmin.getColumn("Nombre");    //Se llama a la columna
+                Nombre.setPreferredWidth(250);                                   //Se define su tamaño
+                TableColumn Apellido = TablaAdminUsrAdmin.getColumn("Apellido");//Se llama a la columna
+                Apellido.setPreferredWidth(250);                                 //Se define su tamaño
+                TableColumn Rol = TablaAdminUsrAdmin.getColumn("Rol");          //Se llama a la columna
+                Rol.setPreferredWidth(160);                                     //Se define su tamaño
+                TableColumn Valorauto = TablaAdminUsrAdmin.getColumn("Valor de autorización");        //Se llama a la columna
+                Valorauto.setPreferredWidth(110);                                  //Se define su tamaño
+                
+            }
+            else                                                                //Si el resultado se encontraba vacío
+            {
+                modeloBajasInt.addColumn("No se encontraron inconsistencias");  //Se crea una columna con la leyecnda
+                TablaAdminUsrAdmin.setModel(modeloBajasInt);                         //Se define el modelo 
+            }
+            
+            conLocal.Cerrar();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        
+        
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,17 +114,80 @@ public class AdministradoresSAP extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel11 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/telcel.jpg"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel11Layout = new javax.swing.GroupLayout(jPanel11);
+        jPanel11.setLayout(jPanel11Layout);
+        jPanel11Layout.setHorizontalGroup(
+            jPanel11Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel11Layout.createSequentialGroup()
+                .addContainerGap(476, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 346, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel11Layout.setVerticalGroup(
+            jPanel11Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel11Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1))
+                    .addComponent(jPanel11, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel11, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 291, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 135, Short.MAX_VALUE))))
         );
 
         pack();
@@ -79,5 +229,10 @@ public class AdministradoresSAP extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel11;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
