1ce8fe73d3d9263c288336133c7a5586fce930f0
YOSH <YOSH@VAIO>
2016-04-30 23:08:57 -0500

    Se hace corección a subquery

--- src/Monitoreos/Tablas.java
+++ src/Monitoreos/Tablas.java
@@ -359,9 +359,9 @@ public class Tablas
     {
         String statement = null;
         String[] parametros = cadenaBD.split("\\|");
-        statement = "INSERT IGNORE INTO AdminUsrAdminGenSAP Usuario, nombre, apellido, rol, valor_autorizacion values (SELECT * FROM usradmin" + parametros[4] + " where usuario not in "
+        statement = "INSERT IGNORE INTO AdminUsrAdminGenSAP (Usuario, nombre, apellido, rol, valor_autorizacion) SELECT * FROM usradmin" + parametros[4] +  " where usuario not in "
                 + "(select numeroempleado as usuario from idint" + parametros[4] + ") and usuario not in (select NUMEROEMPLEADO as usuario2 from "
-                + "idext" + parametros[4] + "))";
+                + "idext" + parametros[4] + ")";
         return statement;
     }
     
