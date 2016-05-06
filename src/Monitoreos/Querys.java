538e11d0bcdcdd2f086cf6a4907b3c6e40192134
VS3XXBD <VS3XXBD@VS3XXBD08ADMXCO.telcel.net>
2016-05-06 14:52:08 -0500

    Se corrige eliminación de métodos de resultados de PAC

--- src/Monitoreos/Querys.java
+++ src/Monitoreos/Querys.java
@@ -39,6 +39,22 @@ public class Querys
         return consulta;
     }
     
+    public static String ResultadosBajasInt()                                   //OBTIENE EL RESULTADO DE LAS INCIDENCIAS DE USUARIOS INTERNOS DADOS DE BAJA
+    {
+        String statement = "select NUM_EMP, USER_NAME, NOMBRE, FECHA_ACCESO, REGION, IP, PERFIL, NOMBRE_PERFIL, IDNUMEMP, IDPUESTO, IDGERENCIA, IDNOMBRE, IDFECHA, "
+                + "IDESTATUS  from baajasint union select NUM_EMP, USER_NAME, NOMBRE, FECHA_ACCESO, REGION, IP, PERFIL, NOMBRE_PERFIL, IDNUMEMP, IDPUESTO, IDGERENCIA, "
+                + "IDNOMBRE, IDFECHA, IDESTATUS  from nonominaint";
+        return statement;
+    }
+    
+    public static String ResultadosBajasExt()                                   //OBTIENE EL RESULTADO DE LAS INCIDENCIAS DE USUARIOS EXTERNOS DADOS DE BAJA
+    {
+        String statement = "select NUM_EMP, USER_NAME, NOMBRE, FECHA_ACCESO, REGION, IP, PERFIL, NOMBRE_PERFIL, IDNUMEMP, IDPUESTO, IDGERENCIA, "
+                + "IDNOMBRE, IDFECHA, IDESTATUS  from baajasext union select NUM_EMP, USER_NAME, NOMBRE, FECHA_ACCESO, REGION, IP, PERFIL, "
+                + "NOMBRE_PERFIL, IDNUMEMP, IDPUESTO, IDGERENCIA, IDNOMBRE, IDFECHA, IDESTATUS  from nonominaext";
+        return statement;
+    }
+    
     
     
                         //******************SAP****************
